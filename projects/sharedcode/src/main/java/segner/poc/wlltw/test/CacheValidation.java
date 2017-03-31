e08742f8d652550ba0581e7dea73030393e7f595
Aaron Segner <asegner@gmail.com>
2017-03-31 12:13:49 -0400

    added a @cacheable thats fired during deployment

---
+++ projects/sharedcode/src/main/java/segner/poc/wlltw/test/CacheValidation.java
@@ -0,0 +1,42 @@
+package segner.poc.wlltw.test;
+
+
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component("cacheValidation")
+public class CacheValidation implements SmartInitializingSingleton {
+
+    private static final Logger LOGGER = Logger.getLogger(CacheValidation.class.getName());
+    private int invocationCount = 0;
+
+    @Override
+    public void afterSingletonsInstantiated() {
+
+        cacheableMethodInSameClass();
+        cacheableMethodInSameClass();
+        clearCache();
+
+        LOGGER.info("Load time weaving cache test passed.");
+
+    }
+
+    @CacheEvict(value = "default", allEntries = true)
+    private void clearCache() {//empty method just for clearing cache
+
+    }
+
+    @Cacheable(value = "default")
+    private int cacheableMethodInSameClass() {
+
+        invocationCount++;
+        if (invocationCount > 1) { // true if we get here twice...
+            throw new RuntimeException("Cache is disabled or Load Time Weaving is not working properly.");
+        }
+        return 0;
+    }
+}
